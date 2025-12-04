6ea0cb2fcdb0564569fef27f9353ff7f59fa8255
Wendy <wgerrar2002@gmail.com>
2025-12-04 11:17:29 +0000

    jsx

--- movie-dashboard/src/utils/localStorage.js
+++
@@ -1,7 +0,0 @@
-const KEY = "movie_favorites";
-
-export const loadFavs = () =>
-  JSON.parse(localStorage.getItem(KEY) || "[]");
-
-export const saveFavs = (arr) =>
-  localStorage.setItem(KEY, JSON.stringify(arr));
